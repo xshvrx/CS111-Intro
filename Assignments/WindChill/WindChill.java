5467592872b4ac37cf46ba371f96101754ce7230
shivp124 <82459557+shivp124@users.noreply.github.com>
2021-08-22 18:46:12 -0400

    Update WindChill.java

--- Assignments/WindChill/WindChill.java
+++ Assignments/WindChill/WindChill.java
@@ -26,10 +26,6 @@ public class WindChill {
     	double t = Double.parseDouble(args[0]);
         double v = Double.parseDouble(args[1]);
         double w = 35.74 + 0.6215 * t + (0.4275 * t - 35.75) * Math.pow(v, 0.16);
-        if (t>50 || v>120 || v<3) {
-            System.out.println("Inputs not valid.");
-        } else {
-            System.out.println(w);
-        }
+        System.out.println(w);
 	}
 }
